0291fe07be6a30fc89b8bcbed96b58aba68cfe79
Jordan Papaleo <papaleowebdev@gmail.com>
2021-01-30 12:14:31 -0800

    super basic forms setup for register

---
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Route } from 'svelte-router-spa'
+  import { onMount } from 'svelte'
+  import { user } from '../../stores.js'
+
+  export let currentRoute
+
+  let initialized = false
+
+  // The onMount lifecycle function
+  onMount(() => {
+    // Svelte stores are written to by calling set
+    user.set({
+      name: 'Test User',
+      schoolName: 'Test School',
+    })
+
+    initialized = true
+  })
+</script>
+
+<div>
+  Admin Layout
+
+  {#if initialized}
+    <Route.default {currentRoute} />
+  {/if}
+</div>
