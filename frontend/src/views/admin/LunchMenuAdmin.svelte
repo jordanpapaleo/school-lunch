4f080501fbcc4db2e130a56aeae27d65478ca08f
Jordan Papaleo <papaleowebdev@gmail.com>
2021-01-30 11:22:56 -0800

    added routing

---
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -0,0 +1,8 @@
+<script>
+  const myDate = new Date().getTime()
+</script>
+
+<div>
+  <h1>Lunch Menu Admin</h1>
+  <h2>{myDate}</h2>
+</div>
---
+++ frontend/src/views/public/LunchMenuView.svelte
@@ -0,0 +1,3 @@
+<div>
+  <h1>Lunch Menu View</h1>
+</div>
