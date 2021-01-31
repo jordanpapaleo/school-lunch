585d25ab6abde7ce442f7b81704fdbf5eb6d174a
Jordan Papaleo <papaleowebdev@gmail.com>
2021-01-31 15:43:00 -0800

    Added lunch week routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
+global.base_dir = __dirname
+global.abs_path = function(path) {
+  return global.base_dir + path
+}
+global.include = function(file) {
+  return require(global.abs_path('/' + file))
+}
+
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const usersRouter = require('./routes/users')
 const lunchWeekRouter = require('./routes/lunchWeek')
 
 const app = express()
+
+app.use(cors())
+app.options('*', cors())
 
 app.use(logger('dev'))
 app.use(express.json())
--- backend/routes/lunchWeek.js
+++ backend/routes/lunchWeek.js
@@ -3,17 +3,17 @@ const router = express.Router()
 
 const lunchWeekList = [
   {
-    lunchWeekId: 1,
+    id: 1,
     weekOf: '2020-10-05',
     isPublished: true,
   },
   {
-    lunchWeekId: 2,
+    id: 2,
     weekOf: '2020-10-12',
     isPublished: true,
   },
   {
-    lunchWeekId: 3,
+    id: 3,
     weekOf: '2020-10-19',
     isPublished: false,
   },
@@ -25,7 +25,7 @@ router.get('/', function(req, res) {
 
 router.get('/:id', function(req, res) {
   const id = parseInt(req.params.id)
-  const lunchWeek = lunchWeekList.find((x) => x.lunchWeekId === id)
+  const lunchWeek = lunchWeekList.find((x) => x.id === id)
 
   if (lunchWeek) {
     res.send(lunchWeek)
@@ -35,3 +35,42 @@ router.get('/:id', function(req, res) {
 })
 
 module.exports = router
+
+/*
+type LunchWeekT = {
+  lunchDayId: Number  //  The primary key of the lunch day entity
+  lunchWeekId: Number  // Foreign key to the parent lunch week entity
+  day: Date  // The date of the given lunch day
+  menuDetails: string  // The details of the daily lunch menu
+}
+
+type LunchWeekT = {
+  lunchWeekId: Number, //  The primary key of the lunch week entity(more on this later)
+  weekOf: Date, // The start date of the school week, e.g. 2020 - 01 - 01
+  isPublished: Boolean, //  Whether or not the week is published for public users to see
+  lunchDays: LunchWeekT[], // A list of child lunchDay objects
+}
+
+{
+ lunchWeek: {
+   lunchWeekId: 1,
+   weekOf: '2020-10-05',
+   isPublished: false,
+   lunchDays: [
+     {
+       lunchDayId: 1,
+       lunchWeekId: 1,
+       day: '2020-10-05',
+       menuDetails: 'Beef tacos or cheese quesadillas with apple slices, rice and black beans'
+     },
+     {
+       lunchDayId: 2,
+       lunchWeekId: 1,
+       day: '2020-10-06',
+       menuDetails: 'Cheese or pepperoni pizza with grapes and celery sticks'
+     }
+   ]
+ }
+}
+
+*/
